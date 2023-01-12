254f31039c8cfd1bfdd5dc800dc9ae3fd8db3468
Source__ <55603482+minseochoi00@users.noreply.github.com>
2023-01-12 01:36:17 -0500

    Update thisiswin11.ps1

--- thisiswin11.ps1
+++ thisiswin11.ps1
@@ -1,5 +1,8 @@
 Write-Host "Installing Scoop for Installation of ThisIsWin11"
 iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
+scoop install git
 scoop bucket add extras
 Write-Host "Installing ThisIsWin11-Latest"
 scoop install thisiswin11
+scoop uninstall scoop
+del .\scoop -Force
