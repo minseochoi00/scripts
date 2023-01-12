2b952586c0d9bc0e65d8f1e1cd95137af40ec621
Source__ <55603482+minseochoi00@users.noreply.github.com>
2023-01-12 01:20:43 -0500

    Update fix-spooler.ps1

--- fix-spooler.ps1
+++ fix-spooler.ps1
@@ -1,6 +1,8 @@
+Write-Host "Stopping Print Spooler Service"
 Stop-Service -Name Spooler -Force
 
-# To delete the files
+Write-Host "Removing Spool System Files"
 Remove-Item -Path "$env:SystemRoot\System32\spool\PRINTERS\*.*"
- 
+
+Write-Host "Starting Print Spooler Service"
 Start-Service -Name Spooler
