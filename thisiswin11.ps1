affa680e4cd351d09c72d73cf20e424cbb06dec9
Source__ <55603482+minseochoi00@users.noreply.github.com>
2023-01-13 18:36:08 -0500

    Update thisiswin11.ps1
    Added Comments

--- thisiswin11.ps1
+++ thisiswin11.ps1
@@ -1,10 +1,24 @@
+#Run this with PowerShell / PowerShell ISE
 Write-Host "Installing Scoop for Installation of ThisIsWin11"
+
+# Installing Scoop
 iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
 Add-MpPreference -ExclusionPath "$($env:programdata)\scoop", "$($env:scoop)"
+
+# Installing GIT & Essentials
 scoop install git
 scoop bucket add extras
+
+# Installing ThisIsWin11
 Write-Host "Installing ThisIsWin11-Latest"
 scoop install thisiswin11
+
+# Waiting for Uninstall of ThisIsWin11 & Scoop
+Write-Host "Pausing for Uninstallation."
+Write-Host "Please close ThisIsWin11 for uninstallation"
+Pause
+
+# Uninstalling Scoop
 Remove-MpPreference -ExclusionPath "$($env:programdata)\scoop", "$($env:scoop)"
 scoop uninstall scoop
 del .\scoop -Force
