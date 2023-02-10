4341edeb9027777ed6bcec041d56a8cf2c9688c7
Source__ <55603482+minseochoi00@users.noreply.github.com>
2023-02-09 19:32:34 -0500

    Update thisiswin11.ps1
    updated that default path wasn't specified
    updated auto unzip file and auto start

--- thisiswin11.ps1
+++ thisiswin11.ps1
@@ -1,6 +1,20 @@
+$DefaultPath = "C:\Data"
+if (Test-Path $DefaultPath) {
+    # NEED THIS LINE BELOW
+    Import-Module BitsTransfer
+} else {
+    # Make Directory @ "C" Drive 
+    mkdir "C:\Data"
+    # NEED THIS LINE BELOW
+    Import-Module BitsTransfer
+}
+
+$NewPath = "$DefaultPath\TIW11-Install\ThisIsWin11.exe"
+
 Write-Host "Downloading This is Win 11"
 # ONLY DOWNLOAD ThisIsWin11 on Windows 11 [ Not Funtionable in Windows 10 ]
 $url="https://github.com/builtbybel/ThisIsWin11/releases/download/1.4.1/TIW11.zip"
-$Path="$DefaultPath\TIW11-Install.TIW11.zip"
+$Path="$DefaultPath\TIW11-Install.zip"
 Start-BitsTransfer -Source $url -Destination $path
-Start-Process "$Path"
+Expand-Archive -Path "$DefaultPath\TIW11-Install.zip" -DestinationPath "$DefaultPath\TIW11-Install"
+Start-Process "$NewPath"
