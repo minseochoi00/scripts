32c38658e8d7b1181797cc250d706fc781159c5b
Source__ <55603482+minseochoi00@users.noreply.github.com>
2023-02-12 12:10:58 -0500

    Update install-winget.ps1
    added new script from Chirs Titus's Script for installation of winget

--- install-winget.ps1
+++ install-winget.ps1
@@ -1,5 +1,46 @@
-Write-Host "Downloading Windows Package Manager"
-$url="https://github.com/microsoft/winget-cli/releases/download/v1.4.10173/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-$Path="$DefaultPath\WPM-Install.msixbundle"
-Start-BitsTransfer -Source $url -Destination $path
-Start-Process "$Path"
+# Check if winget is installed
+    Write-Host "Checking if Winget is Installed..."
+        if (Test-Path ~\AppData\Local\Microsoft\WindowsApps\winget.exe) {
+            #Checks if winget executable exists and if the Windows Version is 1809 or higher
+            Write-Host "Winget Already Installed"
+        } else {
+            #Gets the computer's information
+            $ComputerInfo = Get-ComputerInfo
+
+            #Gets the Windows Edition
+            $OSName = if ($ComputerInfo.OSName) {
+                $ComputerInfo.OSName
+            } else {
+                $ComputerInfo.WindowsProductName
+            }
+
+            if (((($OSName.IndexOf("LTSC")) -ne -1) -or ($OSName.IndexOf("Server") -ne -1)) -and (($ComputerInfo.WindowsVersion) -ge "1809")) {
+                
+                Write-Host "Running Alternative Installer for LTSC/Server Editions"
+
+                # Switching to winget-install from PSGallery from asheroto
+                # Source: https://github.com/asheroto/winget-installer
+                
+                Start-Process powershell.exe -Verb RunAs -ArgumentList "-command irm https://raw.githubusercontent.com/ChrisTitusTech/winutil/$BranchToUse/winget.ps1 | iex | Out-Host" -WindowStyle Normal
+                
+            }
+            elseif (((Get-ComputerInfo).WindowsVersion) -lt "1809") {
+                # Checking if Windows Version is too old for winget
+                Write-Host "Winget is not supported on this version of Windows (Pre-1809)"
+            } else {
+                #Installing Winget from the Microsoft Store
+                Write-Host "Winget not found, installing it now."
+                Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
+                $nid = (Get-Process AppInstaller).Id
+                Wait-Process -Id $nid
+                Write-Host "Winget Installed"
+            }
+        }
+<#
+    Write-Host "Downloading Windows Package Manager"
+    $url="https://github.com/microsoft/winget-cli/releases/download/v1.4.10173/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+    $Path="$DefaultPath\WPM-Install.msixbundle"
+    Start-BitsTransfer -Source $url -Destination $path
+    Start-Process "$Path"
+
+#>
