dea18b4695a9aa3e484c2e0170fdfb027e4bb5ff
Source__ <55603482+minseochoi00@users.noreply.github.com>
2023-02-12 12:23:07 -0500

    Update install-choco.ps1
    updated that chocolatey will not be installed when it is already installed.
    and it will perform update even if it was installed.

--- install-choco.ps1
+++ install-choco.ps1
@@ -1,3 +1,11 @@
-Write-Host "Installing Chocolatey"
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco feature enable -n allowGlobalConfirmation
+# Check if chocolatey is installed and get its version
+if ((Get-Command -Name choco -ErrorAction Ignore) -and ($chocoVersion = (Get-Item "$env:ChocolateyInstall\choco.exe" -ErrorAction Ignore).VersionInfo.ProductVersion)) {
+    Write-Output "Checking for Chocolatey Update"
+    powershell choco feature enable -n allowGlobalConfirmation
+    choco upgrade chocolatey -y
+    Write-Output "Chocolatey Version $chocoVersion is already installed or has been updated"
+} else {
+    Write-Output "Seems Chocolatey is not installed, installing now"
+    Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    powershell choco feature enable -n allowGlobalConfirmation
+}
