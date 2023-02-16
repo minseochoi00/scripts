f6f51e7316df0575fad9ab10bd96b78f37fd8763
Source__ <55603482+minseochoi00@users.noreply.github.com>
2023-02-16 18:10:21 -0500

    Update install-choco.ps1
    added space between write-host to show more clearly to client

--- install-choco.ps1
+++ install-choco.ps1
@@ -1,10 +1,13 @@
 # Check if chocolatey is installed and get its version
 if ((Get-Command -Name choco -ErrorAction Ignore) -and ($chocoVersion = (Get-Item "$env:ChocolateyInstall\choco.exe" -ErrorAction Ignore).VersionInfo.ProductVersion)) {
+    Write-Output ""
     Write-Output "Checking for Chocolatey Update"
     powershell choco feature enable -n allowGlobalConfirmation
     choco upgrade chocolatey -y
+    Write-Output ""
     Write-Output "Chocolatey Version $chocoVersion is already installed or has been updated"
 } else {
+    Write-Output ""
     Write-Output "Seems Chocolatey is not installed, installing now"
     Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
     powershell choco feature enable -n allowGlobalConfirmation
