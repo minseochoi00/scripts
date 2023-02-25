4e666980dfcee675fbd8568c57eed609d6139920
Source__ <55603482+minseochoi00@users.noreply.github.com>
2023-02-25 09:32:47 -0500

    Update thisiswin11.ps1
    Added version variable for easy maintenance and updates.
    # Note this update is a beta version; there could be an error message.

--- thisiswin11.ps1
+++ thisiswin11.ps1
@@ -1,19 +1,20 @@
 $DefaultPath = "C:\Data"
+$Version = "1.4.1"
+$NewPath = "$DefaultPath\TIW11-Install\ThisIsWin11.exe"
+
 if (Test-Path $DefaultPath) {
     # NEED THIS LINE BELOW
     Import-Module BitsTransfer
 } else {
-    # Make Directory @ "C" Drive 
+    # Make Directory @ "C" Drive with Folder name "Data"
     mkdir "C:\Data"
     # NEED THIS LINE BELOW
     Import-Module BitsTransfer
 }
 
-$NewPath = "$DefaultPath\TIW11-Install\ThisIsWin11.exe"
-
-Write-Host "Downloading This is Win 11"
+Write-Host "Downloading, Extracting, Starting ThisisWin 11 Software"
 # ONLY DOWNLOAD ThisIsWin11 on Windows 11 [ Not Funtionable in Windows 10 ]
-$url="https://github.com/builtbybel/ThisIsWin11/releases/download/1.4.1/TIW11.zip"
+$url="https://github.com/builtbybel/ThisIsWin11/releases/download/$Version/TIW11.zip"
 $Path="$DefaultPath\TIW11-Install.zip"
 Start-BitsTransfer -Source $url -Destination $path
 Expand-Archive -Path "$DefaultPath\TIW11-Install.zip" -DestinationPath "$DefaultPath\TIW11-Install"
