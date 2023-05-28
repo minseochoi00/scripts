836432492f93887cd09232e711f51d745a0a02f5
Source__ <55603482+minseochoi00@users.noreply.github.com>
2023-05-28 10:00:50 -0400

    Update fix-spooler.ps1
    added a check for admin

--- fix-spooler.ps1
+++ fix-spooler.ps1
@@ -1,3 +1,18 @@
+# Function to check if the script is running with administrative privileges
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    $isAdmin = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+    return $isAdmin
+}
+
+# Check if the script is running as an administrator
+if (-not (Test-Admin)) {
+    Write-Host "This script needs to be run as an administrator."
+    Write-Host "Please relaunch the script with administrative privileges."
+    Pause
+    Exit
+}
+
 try {
 
     Write-Host "Stopping Print Spooler Service"
