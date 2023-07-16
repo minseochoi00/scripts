d65ea278b712e42e6ce6b34fdc61824a224f3b08
Source__ <55603482+minseochoi00@users.noreply.github.com>
2023-07-16 12:52:39 -0400

    Update fix-spooler.ps1

--- fix-spooler.ps1
+++ fix-spooler.ps1
@@ -1,31 +1,38 @@
 # ENV
+    # General
+        $stop = 'pause'
     # Create New Function 'Test-Admin'
-    function Test-Admin {
-        $currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
-        $isAdmin = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-        return $isAdmin
-    }
-
-    $PrintSpooler_PATH = "$env:SystemRoot\System32\spool\PRINTERS\*.*"
+        function Test-Admin {
+            $currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+            $isAdmin = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+            return $isAdmin
+        }
+    # Print Spooler PATH
+        $PrintSpooler_PATH = "$env:SystemRoot\System32\spool\PRINTERS\*.*"
 
 # ------------------------------------------------------------------------------------------
+
 # Check if the script is running as an administrator
 if (-not (Test-Admin)) {
-    Write-Host "This script needs to be run as an administrator."
-    Write-Host "Please relaunch the script with administrative privileges."
+    Write-Host "This Code requires Administrative Privileges."
     Pause
     return
     Exit
 }
 
 # Start
+
 try {
 
     Write-Host "Stopping Print Spooler Service"
-    Stop-Service -Name Spooler -Force
+    if (Get-Service -Name Spooler) { Stop-Service -Name Spooler -Force }
 
     Write-Host "Removing Spool System Files"
-    if (Test-Path $PrintSpooler_PATH) { Remove-Item -Path $PrintSpooler_PATH -ErrorAction Ignore } else { Write-Output "PATH does not EXIST." }
+    if (Test-Path $PrintSpooler_PATH) { Remove-Item -Path $PrintSpooler_PATH -ErrorAction Ignore } 
+    else { 
+        Write-Output "$PrintSpooler_PATH" 
+        Write-Output "Above PATH does not EXIST." 
+    }
 
     Write-Host "Starting Print Spooler Service"
     Start-Service -Name Spooler
@@ -34,13 +41,15 @@ try {
     Return
 
 }
+
 # End
 
 # Error Prompt
 catch {
 
-    Write-Host "Error has occured while Fixing Print Spooler"
-    Pause
+    Write-Output "Error has occured while Fixing Print Spooler"
+    Write-Output "Please 'RE-START' the workstation."
+    $stop
     Return
 
 }
