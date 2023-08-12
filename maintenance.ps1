1d80f9681ec96970cbde9cf406b410f107a3821e
Source__ <55603482+minseochoi00@users.noreply.github.com>
2023-08-11 20:31:54 -0400

    2023-08-11
    cleanup and rebuild

--- maintenance.ps1
+++ maintenance.ps1
@@ -3,7 +3,7 @@
 # env
     # Choco
     $Test_Choco = Get-Command -Name choco -ErrorAction Ignore
-        # Check for Chocolatey and Winget Installation
+        # Check for Chocolatey Installation if can't be found install it.
             if (-not($Test_Choco)) { irm minseochoi.tech/script/install-choco | iex }
     # Print Spooler
         $PrintSpooler_PATH = "$env:SystemRoot\System32\spool\PRINTERS\*.*"
@@ -12,31 +12,35 @@
     # OneDrive
         $Process_oneDrive = Get-Process -Name OneDrive -ErrorAction SilentlyContinue
     # NTP Server Tweaks
-        $serviceName = "W32Time"
-        $service = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
+        $NTPserviceName = "W32Time"
+        $NTPservice = Get-Service -Name $NTPserviceName -ErrorAction SilentlyContinue
+    # Check if the current user has administrative privileges
+        $isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    
         
 # ------------------------------------------------------------------------------------------------------------------------
 
 # Start
 
 # Stop File Explorer
-    Write-Host "Stopping Windows Explorer..."
+    Write-Host -NoNewLine "Stopping Windows Explorer..."
     if (Get-Process -Name Explorer -ErrorAction SilentlyContinue) { taskkill /f /im explorer.exe }
+    if (Get-Process -Name Explorer -ErrorAction SilentlyContinue) { Write-Host " (Stopped)"} else { Write-Host " (Failed)"}
 
 # Set Execution Policy
     if (-not (Get-ExecutionPolicy) -eq "Bypass") { Set-ExecutionPolicy Bypass -Force }
 
 # Set PSGallery as Trusted
-    Start-Process powershell.exe -ArgumentList "Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted" -Verb RunAs -ErrorAction Ignore
+    Start-Process powershell.exe -ArgumentList "Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted" -Verb RunAs -Wait
 
 # Installing NuGet Package
-    Start-Process powershell.exe -ArgumentList "Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Confirm $false" -Verb RunAs -ErrorAction Ignore
+    Start-Process powershell.exe -ArgumentList "Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Confirm $false" -Verb RunAs -Wait
 
 # Installing Windows Update Module
-    Start-Process powershell.exe -ArgumentList "Install-Module PSWindowsUpdate -Confirm $false"  -Verb RunAs -ErrorAction Ignore
+    Start-Process powershell.exe -ArgumentList "Install-Module PSWindowsUpdate -Confirm $false"  -Verb RunAs -Wait
     
 # Importing Windows Update Module
-    Start-Process powershell.exe -ArgumentList "Import-Module PSWindowsUpdate -Confirm $false"  -Verb RunAs -ErrorAction Ignore
+    Start-Process powershell.exe -ArgumentList "Import-Module PSWindowsUpdate -Confirm $false"  -Verb RunAs -Wait
     
 # Required Parameter for Disk Clean-up
     $SageSet = "StateFlags0099"
@@ -80,11 +84,12 @@
 #### Start
 
 # Check for One-Drive Installation
+    Write-Host -NoNewLine "Checking if OneDrive is running in background"
     if ($Process_oneDrive) {
-        Write-Output "OneDrive is currently "RUNNING", "STARTING Uninstaller""
+        Write-Host " (Found. Starting Auto-Removal)"
         Start-Process powershell.exe -ArgumentList "irm minseochoi.tech/script/remove-onedrive | iex" -Verb RunAs -ErrorAction SilentlyContinue
     } else {
-        Write-Output "OneDrive is currently "NOT RUNNING" on this workstation."
+        Write-Host " (Has not found.)"
     }
 
 
@@ -95,7 +100,7 @@
 
 # Delete Windows update files
     Write-Host "Deleting Windows update files..."
-    Remove-Item $WindowsUpdateFolder\* -Recurse -Force -ErrorAction SilentlyContinue
+    Remove-Item '$WindowsUpdateFolder\*' -Recurse -Force -ErrorAction SilentlyContinue
 
 # Delete old Windows installation files
     Write-Host "Deleting old Windows installation files..."
@@ -103,8 +108,9 @@
     catch { Write-Host "Error has occured while deleting old Windows installation files." }
     
 # Flush Cache
-    Write-Host "Flushing IP Cache"
-    Start-Process -FilePath ipconfig -ArgumentList "/flushdns" -WindowStyle Hidden
+    Write-Host -NoNewLine "Flushing IP Cache"
+    Start-Process -FilePath ipconfig -ArgumentList "/flushdns" -WindowStyle Hidden -Wait
+    Write-Host " (Finished)"
 
 # Empty Recycle Bin
     Write-Host "Empty Recycle Bin"
@@ -116,76 +122,56 @@
         Remove-Item -Path $PrintSpooler_PATH -ErrorAction Ignore
         Start-Service -Name Spooler
 
-# Fix NTP Server
-    Write-Host "Fixing Workstation's NTP Server"
-    if ($null -eq $service) { Start-Service -Name $serviceName }
-    Start-Process -FilePath w32tm -ArgumentList "/config /manualpeerlist:time.google.com /syncfromflags:MANUAL /reliable:yes /update" -WindowStyle Hidden
-    Restart-Service -Name $serviceName
-    Start-Process -FilePath w32tm -ArgumentList "/config /update" -WindowStyle Hidden
-    Start-Process -FilePath w32tm -ArgumentList "/resync /nowait /rediscover" -WindowStyle Hidden
-
+    # Windows NTP Server Tweaks
+    Write-Host -NoNewLine "Fixing Workstation's NTP Server"
+    if (-not($isAdmin)) {Write-Host " (Failed: Permission)"}
+    else {
+        if (($NTPservice).Status -eq 'Stopped') { Start-Service -Name $NTPserviceName }
+        Start-Process -FilePath w32tm -ArgumentList "/config /manualpeerlist:time.google.com /syncfromflags:MANUAL /reliable:yes /update" -WindowStyle Hidden
+        Restart-Service -Name $NTPserviceName
+        Start-Process -FilePath w32tm -ArgumentList "/config /update" -WindowStyle Hidden
+        Start-Process -FilePath w32tm -ArgumentList "/resync /nowait /rediscover" -WindowStyle Hidden
+        Write-Host " (Done)"
+    }
 
 # Resync Time
     Write-Host "Resyncing Time"
     Start-Process -FilePath w32tm -ArgumentList "/resync /nowait /rediscover" -WindowStyle Hidden
 
 # Running Disk Cleanup
-    Write-Host "Starting Disk Cleanup"
-    # -ea silentlycontinue will supress error messages
-    ForEach ($Location in $Locations) {
-        Set-ItemProperty -Path $($Base+$Location) -Name $SageSet -Type DWORD -Value 2 -ea silentlycontinue | Out-Null
-    }
+    Write-Host -NoNewLine "Starting Disk Cleanup"
+        ForEach ($Location in $Locations) { Set-ItemProperty -Path $($Base+$Location) -Name $SageSet -Type DWORD -Value 2 -ea silentlycontinue | Out-Null }
+        # Do the clean-up. Have to convert the SageSet number
+            $Args = "/sagerun:$([string]([int]$SageSet.Substring($SageSet.Length-4)))"
+            Start-Process -Wait "$env:SystemRoot\System32\cleanmgr.exe" -ArgumentList $Args -Wait
+        # Remove the Stateflags
+            ForEach($Location in $Locations) { Remove-ItemProperty -Path $($Base+$Location) -Name $SageSet -Force -ea silentlycontinue | Out-Null }
+        # Output message that it has been finished
+            Write-Host " (Finished)"
 
-    # Do the clean-up. Have to convert the SageSet number
-    $Args = "/sagerun:$([string]([int]$SageSet.Substring($SageSet.Length-4)))"
-    Start-Process -Wait "$env:SystemRoot\System32\cleanmgr.exe" -ArgumentList $Args
 
-    # Remove the Stateflags
-    ForEach($Location in $Locations) {
-    Remove-ItemProperty -Path $($Base+$Location) -Name $SageSet -Force -ea silentlycontinue | Out-Null
-    }
 
 # Check and repair system files
-    Write-Host "Checking and repairing system files..."
-    Start-Process -FilePath sfc -ArgumentList "/scannow" -WindowStyle Hidden
+    Write-Host -NoNewLine "Checking and repairing system files..."
+        Start-Process -FilePath sfc -ArgumentList "/scannow" -WindowStyle Hidden -Wait
+        # Output message that it has been finished
+            Write-Host " (Finished)"
 
 # Windows Update
-    Write-Host "Checking for Windows Update"
+    Write-Host -NoNewLine "Checking for Windows Update"
     # Check for Windows updates (excluding drivers)
-    Get-WindowsUpdate -Download -Hide -IgnoreReboot -NotCategory "Drivers" -ErrorAction SilentlyContinue
+        $Args = 'Get-WindowsUpdate -Download -Hide -IgnoreReboot -NotCategory "Drivers"'
+        Start-Process -FilePath powershell -ArgumentList $Args -Verb RunAs -WindowStyle Hidden -Wait
+    # Output message that it has been finished
+        Write-Host " (Finished)"       
+    
    
 # Starting File Explorer
-    Write-Host "Starting Windows Explorer..."
-    if (-not(Get-Process -Name explorer -ErrorAction SilentlyContinue)) { Start-Process Explorer.exe }
-
-# Prompt user to reboot
-    Read-Host -Prompt "Cleanup completed. Do you want to reboot now? (Y/N)"
-    if ($rebootChoice.ToUpper() -eq "Y") {
-        Restart-Computer -Force
-    } elseif ($rebootChoice.ToUpper() -eq "Yes") {
-        Restart-Computer -Force
-    } elseif ($rebootChoice.ToUpper() -eq "yes") {
-        Restart-Computer -Force
-    } else {
-        Write-Host "You can manually reboot your computer later at your convenience."
-    }
-
-# Prompt user to reboot
-    do {
-        $Restart_Choice = Read-Host -Prompt "$computerName / $userName is recommended to restart. Would you like to perform a restart? "
-        if ($Restart_Choice.ToUpper() -eq "YES" -or $Restart_Choice.ToUpper() -eq "Y" -or $Restart_Choice.ToUpper() -eq "y" -or $Restart_Choice.ToUpper() -eq "yes") { $Restart = $true } 
-        elseif ($Restart_Choice.ToUpper() -eq "NO" -or $Restart_Choice.ToUpper() -eq "N" -or $Restart_Choice.ToUpper() -eq "no" -or $Restart_Choice.ToUpper() -eq "n") { $Restart = $false } 
-        else {
-            Write-Host "You must select either Yes (Y) or No (N)." 
-        }
-    } while (-not ($Restart -eq $true -or $Restart -eq $false))
-
-# Performing from User Prompt
-    if ($restart) { Restart-Computer -Force }
-    if (-not($restart)) { Write-Host "Please reboot/shutdown your computer for these cleanup to affect ASAP." }
+    Write-Host -NoNewLine "Re-starting Windows Explorer..."
+    if (-not(Get-Process -Name explorer -ea SilentlyContinue)) { Start-Process Explorer.exe }
+    if (Get-Process -Name Explorer -ea SilentlyContinue) { Write-Host " (Started)"} else { Write-Host " (Failed: Start)"}
 
 # Exit
-    $Stop
-    Exit
+    return
 
 # End
