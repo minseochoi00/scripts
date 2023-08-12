752987ac2d33a0ef89ebe8f210a6888ef471f712
Minseo Choi <55603482+minseochoi00@users.noreply.github.com>
2023-08-12 01:00:46 -0400

    Update remove-onedrive.ps1

--- remove-onedrive.ps1
+++ remove-onedrive.ps1
@@ -1,59 +1,68 @@
-Import-Module -DisableNameChecking $PSScriptRoot\..\lib\force-mkdir.psm1 -ErrorAction Ignore
-Import-Module -DisableNameChecking $PSScriptRoot\..\lib\take-own.psm1 -ErrorAction Ignore
+Import-Module -DisableNameChecking $PSScriptRoot\..\lib\force-mkdir.psm1 -ea Ignore
+Import-Module -DisableNameChecking $PSScriptRoot\..\lib\take-own.psm1 -ea Ignore
 
 Write-Output "Kill OneDrive process"
-taskkill.exe /F /IM "OneDrive.exe"
-taskkill.exe /F /IM "explorer.exe"
+    Start-Process -FilePath "taskkill" -ArgumentList '/f /im "OneDrive.exe"' -Verb RunAs -ea SilentlyContinue
+    Start-Process -FilePath "taskkill" -ArgumentList '/f /im "explorer.exe"' -Verb RunAs -ea SilentlyContinue
 
-Write-Output "Remove OneDrive"
-if (Test-Path "$env:systemroot\System32\OneDriveSetup.exe") {
-    & "$env:systemroot\System32\OneDriveSetup.exe" /uninstall
-}
-if (Test-Path "$env:systemroot\SysWOW64\OneDriveSetup.exe") {
+Write-Output -NoNewLine "Remove OneDrive"
+    if (Test-Path "$env:systemroot\System32\OneDriveSetup.exe") {
+        Write-Host " (Found)"
+        & "$env:systemroot\System32\OneDriveSetup.exe" /uninstall
+    } elseif (Test-Path "$env:systemroot\SysWOW64\OneDriveSetup.exe") {
+        Write-Host " (Found)"
     & "$env:systemroot\SysWOW64\OneDriveSetup.exe" /uninstall
-}
+    } else {
+        Write-Host " (Failed: Can't be found)"
+    }
 
 Write-Output "Removing OneDrive leftovers"
-Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "$env:localappdata\Microsoft\OneDrive"
-Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "$env:programdata\Microsoft OneDrive"
-Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "$env:systemdrive\OneDriveTemp"
+    Remove-Item -Recurse -Force -ea SilentlyContinue "$env:localappdata\Microsoft\OneDrive"
+    Remove-Item -Recurse -Force -ea SilentlyContinue "$env:programdata\Microsoft OneDrive"
+    Remove-Item -Recurse -Force -ea SilentlyContinue "$env:systemdrive\OneDriveTemp"
 
 # check if directory is empty before removing:
-If ((Get-ChildItem "$env:userprofile\OneDrive" -Recurse | Measure-Object).Count -eq 0) {
-    Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "$env:userprofile\OneDrive"
-}
+    If ((Get-ChildItem "$env:userprofile\OneDrive" -Recurse | Measure-Object).Count -eq 0) { Remove-Item -Recurse -Force -ea SilentlyContinue "$env:userprofile\OneDrive" }
 
-Write-Output "Disable OneDrive via Group Policies"
-mkdir -Force "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\OneDrive"
-Set-ItemProperty "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\OneDrive" "DisableFileSyncNGSC" 1
+Write-Output -NoNewLine "Disable OneDrive via Group Policies"
+    mkdir -Force "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\OneDrive"
+    # Using [void] to suppress output
+    try {
+        [void](Set-ItemProperty "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\OneDrive" "DisableFileSyncNGSC" 1)
+        Write-Host " (Successful)"
+    } catch { Write-Host " (Failed)" }
 
-Write-Output "Remove Onedrive from explorer sidebar"
-New-PSDrive -PSProvider "Registry" -Root "HKEY_CLASSES_ROOT" -Name "HKCR"
-mkdir -Force "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}"
-Set-ItemProperty "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" "System.IsPinnedToNameSpaceTree" 0
-mkdir -Force "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}"
-Set-ItemProperty "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" "System.IsPinnedToNameSpaceTree" 0
-Remove-PSDrive "HKCR"
+Write-Output -NoNewLine "Remove Onedrive from explorer sidebar"
+    New-PSDrive -PSProvider "Registry" -Root "HKEY_CLASSES_ROOT" -Name "HKCR"
+    mkdir -Force "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}"
+    try {
+        Set-ItemProperty "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" "System.IsPinnedToNameSpaceTree" 0
+        Write-Host " (Successful 1)"
+    } catch { Write-Host " (Failed 1)" }
+    mkdir -Force "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}"
+    try {
+        Set-ItemProperty "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" "System.IsPinnedToNameSpaceTree" 0
+        Write-Host " (Successful 1)"
+    } catch { Write-Host " (Failed 1)" }
+    Remove-PSDrive "HKCR"
 
-# Thank you Matthew Israelsson
 Write-Output "Removing run hook for new users"
-reg load "hku\Default" "C:\Users\Default\NTUSER.DAT"
-reg delete "HKEY_USERS\Default\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "OneDriveSetup" /f
-reg unload "hku\Default"
+    Start-Process -FilePath reg -ArgumentList 'load "hku\Default" "C:\Users\Default\NTUSER.DAT"' -Verb RunAs -ea SilentlyContinue
+    Start-Process -FilePath reg -ArgumentList 'delete "HKEY_USERS\Default\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "OneDriveSetup" /f' -Verb RunAs -ea SilentlyContinue
+    Start-Process -FilePath reg -ArgumentList 'unload "hku\Default"' -Verb RunAs -ea SilentlyContinue
 
 Write-Output "Removing startmenu entry"
-Remove-Item -Force -ErrorAction SilentlyContinue "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk"
+    Remove-Item -Force -ea SilentlyContinue "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk"
 
 Write-Output "Removing scheduled task"
-Get-ScheduledTask -TaskPath '\' -TaskName 'OneDrive*' -ErrorAction SilentlyContinue | Unregister-ScheduledTask -Confirm:$false
+    Start-Process -FilePath reg -ArgumentList 'unload "hku\Default"' -Verb RunAs -ea SilentlyContinue
+    foreach ($Task in (Get-ScheduledTask -TaskPath '\' -TaskName 'OneDrive*' -ea SilentlyContinue)) { Unregister-ScheduledTask -Confirm:$false }
 
 Write-Output "Restarting explorer"
-Start-Process "explorer.exe"
+    Start-Process "explorer.exe"
 
 Write-Output "Waiting for explorer to complete loading"
-Start-Sleep 10
+    Start-Sleep 10
 
 Write-Output "Removing additional OneDrive leftovers"
-foreach ($item in (Get-ChildItem "$env:WinDir\WinSxS\*onedrive*")) {
-    Remove-Item -Recurse -Force $item.FullName -ErrorAction SilentlyContinue
-}
+    foreach ($item in (Get-ChildItem "$env:WinDir\WinSxS\*onedrive*")) { Remove-Item -Recurse -Force $item.FullName -ea SilentlyContinue }
